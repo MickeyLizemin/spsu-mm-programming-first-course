bdeec9d1db8f8240399e5027505fed82d4fa0de4
Kuznetsov Ilya Alexandrovich <kuznetsov.ilya.alexandrovich@gmail.com>
2019-11-12 17:50:32 +0300

    New changes added in 12th task (v. 2.0)
    1. Fixed non-optimal memory usage by array.
    2. Improving the speed of the program.

--- Completed homeworks/Homework 6/12th_task/12th_task/12th_task.c
+++ Completed homeworks/Homework 6/12th_task/12th_task/12th_task.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 #include <malloc.h>
 
 int main()
 {
 	int* array;
-	array = (int*)malloc(5000 * sizeof(int));
-	int counter_a, counter_b, extra_zero_counter = 0;
+	int counter_a, counter_b, digit_counter;
 
-	for (counter_a = 0; counter_a <= 4999; counter_a++)
+	digit_counter = (int)(5000 * log(3) / log(16)); //We do not add 1 to the result, since the elements of the array are numbered from 0
+	array = (int*)malloc((digit_counter + 1) * sizeof(int)); //Add 1 here in connection with what is written above
+
+	for (counter_a = 0; counter_a <= digit_counter; counter_a++)
 	{
 		array[counter_a] = 0;
 	}
@@ -16,12 +19,12 @@ int main()
 
 	for (counter_a = 0; counter_a <= 4999; counter_a++)
 	{
-		for (counter_b = 0; counter_b <= 4999; counter_b++)
+		for (counter_b = 0; counter_b <= digit_counter; counter_b++)
 		{
 			array[counter_b] = array[counter_b] * 3;
 		}
 
-		for (counter_b = 0; counter_b <= 4998; counter_b++)
+		for (counter_b = 0; counter_b <= digit_counter - 1; counter_b++) //Subtract 1, otherwise it is possible to get out of the array
 		{
 			if (array[counter_b] >= 16)
 			{
@@ -31,21 +34,9 @@ int main()
 		}
 	}
 
-	for (counter_a = 4999; counter_a >= 0; counter_a--)
-	{
-		if (array[counter_a] == 0)
-		{
-			extra_zero_counter++;
-		}
-		else
-		{
-			break;
-		}
-	}
-
 	printf("The number 3^5000 in the hexadecimal number system is:\n");
 
-	for (counter_a = 4999 - extra_zero_counter; counter_a >= 0; counter_a--)
+	for (counter_a = digit_counter; counter_a >= 0; counter_a--)
 	{
 		printf("%X", array[counter_a]);
 	}
